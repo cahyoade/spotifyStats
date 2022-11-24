e210b4043a0a30aaa30658bd06955a33517eb478
Cahyo Ade <cahyoade8@gmail.com>
2022-11-24 19:45:40 +0700

    change redirect url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?response_type=token&client_id=647bf34d7074498aa76c40c655fad809&scope=user-top-read%20user-read-email%20user-read-private%20user-library-read&redirect_uri=http://127.0.0.1:5173/&show_dialog=true';
+const AUTH_URL = 'https://accounts.spotify.com/authorize?response_type=token&client_id=647bf34d7074498aa76c40c655fad809&scope=user-top-read%20user-read-email%20user-read-private%20user-library-read&redirect_uri=https://ta-ppb-141eb.web.app/&show_dialog=true';
 
 export default function Login(){
     return (
